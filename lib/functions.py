1df7bdd03479392b7698e8c65594459589904ff7
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-02-18 05:24:46 -0700

    js to python

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet("name")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default("Sunny")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+add(2, 2)
 
 def halve(number):
-    pass
+    if number != int():
+        return number / 2
+        return None
+halve(4)
---
+++ some_py.py
@@ -0,0 +1,15 @@
+# def my_function(param):
+#     print("Running my_function")
+#     return param + 1
+
+# my_function_return_value = my_function(1)
+# my_function_return_value
+
+# def stylish_painter():
+#     best_hairstyle = "Bob Ross"
+#     return "Jean-Michel Basquiat"
+#     return best_hairstyle
+#     print(best_hairstyle)
+
+# stylish_painter()
+
